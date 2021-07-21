aad177a43f765e86b8026cefb770594ce55ad261
kandha0205 <87473328+kandha0205@users.noreply.github.com>
2021-07-21 12:54:15 +0200

    Revert "Merge branch 'main' of https://github.com/kandha0205/java into main"
    This reverts commit 0f939f116cd5904ae164faeb45c18e945bd4513c, reversing
    changes made to 902f7897c33d7fd2d64a6f091776d70215215258.

--- maven_java/src/main/resources/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("test");
-
-        // adding second line
-        System.out.println("second line");
-    }
-}
